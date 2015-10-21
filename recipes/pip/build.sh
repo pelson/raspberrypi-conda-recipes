aed0a02d314152bf0fb7b2d85ae169a4444a5112
Phil Elson <pelson.pub@gmail.com>
2015-10-21 06:33:20 +0100

    Pip installation without setuptools.

--- recipes/pip/build.sh
+++ recipes/pip/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# $PYTHON setup.py install --single-version-externally-managed --record=record.txt --old-and-unmanageable
-$PYTHON setup.py install
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt --old-and-unmanageable
 
